92339733d20185de5f63121dbd9940d25bf1e177
Johannes 'johno' Norneby <johannes.norneby@gmail.com>
2024-06-17 19:50:32 +0200

    -public: color quantization seems more correct

--- public/minyin/paletas.cpp
+++ public/minyin/paletas.cpp
@@ -53,9 +53,7 @@ static int32_t __sort_b(const void* elem1, const void* elem2)
 	return 0;
 }
 
-static void __sort(
-	const uint32_t in_bucket_start, const uint32_t in_bucket_end,
-	std::vector<pixel_t>& out_pixels)
+static void __sort(std::vector<pixel_t>& out_bucket)
 {
 	int32_t low_r = INT_MAX;
 	int32_t low_g = INT_MAX;
@@ -63,9 +61,8 @@ static void __sort(
 	int32_t high_r = INT_MIN;
 	int32_t high_g = INT_MIN;
 	int32_t high_b = INT_MIN;
-	for (uint32_t i = in_bucket_start; i < in_bucket_end; ++i)
+	for (const pixel_t& PIXEL : out_bucket)
 	{
-		const pixel_t& PIXEL = out_pixels[i];
 		low_r = __min(low_r, PIXEL.r);
 		low_g = __min(low_g, PIXEL.g);
 		low_b = __min(low_b, PIXEL.b);
@@ -80,26 +77,32 @@ static void __sort(
 	//3) sort by greatest range
 	const int32_t GREATEST = __max(RED, __max(GREEN, BLUE));
 	if (GREATEST == RED)
-		::qsort(out_pixels.data() + in_bucket_start, in_bucket_end - in_bucket_start, sizeof(pixel_t), __sort_r);
+		::qsort(out_bucket.data(), out_bucket.size(), sizeof(pixel_t), __sort_r);
 	else if (GREATEST == GREEN)
-		::qsort(out_pixels.data() + in_bucket_start, in_bucket_end - in_bucket_start, sizeof(pixel_t), __sort_g);
+		::qsort(out_bucket.data(), out_bucket.size(), sizeof(pixel_t), __sort_g);
 	else if (GREATEST == BLUE)
-		::qsort(out_pixels.data() + in_bucket_start, in_bucket_end - in_bucket_start, sizeof(pixel_t), __sort_b);
+		::qsort(out_bucket.data(), out_bucket.size(), sizeof(pixel_t), __sort_b);
 	else
 		assert(0);
 }
 
-static void __sort_recursive(
-	const uint32_t in_depth, const uint32_t in_palette_size, const uint32_t in_bucket_start, const uint32_t in_bucket_end,
-	std::vector<pixel_t>& out_pixels)
+static void __split(
+	const std::vector<pixel_t>& in_bucket,
+	std::vector<std::vector<pixel_t>>& out_new_buckets)
 {
-	if (in_depth > in_palette_size)
-		return;
-	if (in_bucket_start >= in_bucket_end)
-		return;
-	__sort(in_bucket_start, in_bucket_end, out_pixels);
-	__sort_recursive(in_depth * 2, in_palette_size, in_bucket_start, in_bucket_end / 2, out_pixels);
-	__sort_recursive(in_depth * 2, in_palette_size, in_bucket_start + in_bucket_end / 2, in_bucket_end, out_pixels);
+	{
+		std::vector<pixel_t> a;
+		for (uint32_t i = 0; i < in_bucket.size() / 2; ++i)
+			a.push_back(in_bucket[i]);
+		out_new_buckets.push_back(a);
+	}
+
+	{
+		std::vector<pixel_t> b;
+		for (uint32_t i = in_bucket.size() / 2; i < in_bucket.size(); ++i)
+			b.push_back(in_bucket[i]);
+		out_new_buckets.push_back(b);
+	}
 }
 
 //public
@@ -140,7 +143,7 @@ bool paletas_calculate(
 	{
 		//1) get all unique pixels
 		uint32_t total_source_pixels = 0;
-		std::vector<pixel_t> unique_pixels_vector;
+		std::vector<std::vector<pixel_t>> all_buckets;
 		{
 			std::set<pixel_t> unique_pixels_set;
 			for (const fs_tga_image_t* SOURCE_IMAGE : source_images)
@@ -155,41 +158,50 @@ bool paletas_calculate(
 					unique_pixels_set.insert(*pixel);
 				}
 			}
+			std::vector<pixel_t> first_bucket;
 			for (const pixel_t& UP : unique_pixels_set)
-				unique_pixels_vector.push_back(UP);
-			assert(unique_pixels_set.size() == unique_pixels_vector.size());
+				first_bucket.push_back(UP);
+			assert(unique_pixels_set.size() == first_bucket.size());
+			all_buckets.push_back(first_bucket);
 		}
 
-		//2) figure out which channel has the greatest range
-		__sort_recursive(1, in_palette_size, 0, unique_pixels_vector.size(), unique_pixels_vector);
-
-		//4) split into buckets (desired number of colors) and average buckets to a single color (these colors will be final palette colors)
-		const float FLOAT_PIXELS_IN_BUCKET = (float)unique_pixels_vector.size() / (float)in_palette_size;
-		const uint32_t PIXELS_IN_BUCKET = uint32_t(FLOAT_PIXELS_IN_BUCKET + .5f);//round
-		for (uint32_t bucket = 0; bucket < in_palette_size; ++bucket)
+		//2) figure out which channel has the greatest range, sort, and split until we have as many buckets as we want palette indices
+		while (all_buckets.size() < in_palette_size)
 		{
+			std::vector<std::vector<pixel_t>> new_buckets;
+			for (std::vector<pixel_t>& bucket : all_buckets)
+			{
+				__sort(bucket);
+				__split(bucket, new_buckets);
+			}
+			all_buckets.clear();
+			for (const std::vector<pixel_t>& NEW_BUCKET : new_buckets)
+				all_buckets.push_back(NEW_BUCKET);
+		}
+		assert(all_buckets.size() == in_palette_size);
+
+		//4) average buckets to a single color (these colors will be final palette colors)
+		for (uint32_t bucket_index = 0; bucket_index < all_buckets.size(); ++bucket_index)
+		{
+			const std::vector<pixel_t>& BUCKET = all_buckets[bucket_index];
 			float r = 0;
 			float g = 0;
 			float b = 0;
-			for (uint32_t p = 0; p < PIXELS_IN_BUCKET; ++p)
+			for (const pixel_t& PIXEL : BUCKET)
 			{
-				const uint32_t PIXEL_INDEX = p + bucket * PIXELS_IN_BUCKET;
-				if (PIXEL_INDEX < unique_pixels_vector.size())
-				{
-					r += unique_pixels_vector[PIXEL_INDEX].r;
-					g += unique_pixels_vector[PIXEL_INDEX].g;
-					b += unique_pixels_vector[PIXEL_INDEX].b;
-				}
+				r += PIXEL.r;
+				g += PIXEL.g;
+				b += PIXEL.b;
 			}
-			r /= (float)PIXELS_IN_BUCKET;
-			g /= (float)PIXELS_IN_BUCKET;
-			b /= (float)PIXELS_IN_BUCKET;
+			r /= (float)BUCKET.size();
+			g /= (float)BUCKET.size();
+			b /= (float)BUCKET.size();
 			assert(r >= 0 && r < 256);
 			assert(g >= 0 && g < 256);
 			assert(b >= 0 && b < 256);
-			palette[bucket].r = (uint8_t)r;
-			palette[bucket].g = (uint8_t)g;
-			palette[bucket].b = (uint8_t)b;
+			palette[bucket_index].r = (uint8_t)r;
+			palette[bucket_index].g = (uint8_t)g;
+			palette[bucket_index].b = (uint8_t)b;
 		}
 	}
 
